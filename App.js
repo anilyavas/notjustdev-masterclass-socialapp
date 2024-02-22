6dce9632acfffb26f49f75b0bd4ba3bd50033d32
Anıl <anil@Anl-MacBook-Air.local>
2024-02-22 14:37:11 +0300

    create post screen

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import FeedScreen from './src/screens/FeedScreen';
 import { StatusBar } from 'expo-status-bar';
 import CreatePostScreen from './src/screens/CreatePostScreen';
 
 export default function App() {
   return (
-    <SafeAreaView styles={styles.container}>
+    <View styles={styles.container}>
       <CreatePostScreen />
       <StatusBar style='auto' />
-    </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#c9c9c9',
   },
 });
--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -1,5 +1,15 @@
 import { useState } from 'react';
-import { View, Text, StyleSheet, Image, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+// import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const user = {
   id: 'u1',
@@ -7,31 +17,47 @@ const user = {
     'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg',
   name: 'Vadim Savin',
 };
+
 const CreatePostScreen = () => {
   const [description, setDescription] = useState('');
+  // const insets = useSafeAreaInsets();
+
+  const onSubmit = () => {
+    console.warn('On Submit: ', description);
+    setDescription('');
+  };
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={[styles.container, { marginBottom: 10 }]}
+      contentContainerStyle={{ flex: 1 }}
+      keyboardVerticalOffset={150}
+    >
       <View style={styles.header}>
         <Image source={{ uri: user.image }} style={styles.profileImage} />
         <Text style={styles.name}>{user.name}</Text>
       </View>
       <TextInput
-        placeholder='What is on your mind?'
-        multiline
+        placeholder="What's on your mind?"
         value={description}
         onChangeText={setDescription}
+        style={styles.input}
+        multiline
       />
-    </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={onSubmit} title='Post' disabled={!description} />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
-
-export default CreatePostScreen;
 
 const styles = StyleSheet.create({
   container: {
     width: '100%',
     padding: 10,
-    marginTop: 20,
+    paddingTop: 30,
+    backgroundColor: 'white',
   },
   header: {
     padding: 10,
@@ -42,10 +68,19 @@ const styles = StyleSheet.create({
   profileImage: {
     width: 40,
     height: 40,
-    borderRadius: 30,
+    borderRadius: 25,
     marginRight: 10,
   },
   name: {
     fontWeight: '500',
   },
+  input: {
+    marginBottom: 'auto',
+  },
+  buttonContainer: {
+    marginTop: 'auto',
+    marginVertical: 10,
+  },
 });
+
+export default CreatePostScreen;
