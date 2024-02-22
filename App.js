e0e1542171ca3f716d1fc32e7cf1cd4a3ef102c4
Anıl <anil@Anl-MacBook-Air.local>
2024-02-22 13:30:43 +0300

    feed screen

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
-import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import FeedScreen from './src/screens/FeedScreen';
 import { StatusBar } from 'expo-status-bar';
-import { FeedPost } from './src/components/FeedPost';
-import posts from './assets/data/posts.json';
 
 export default function App() {
   return (
     <SafeAreaView styles={styles.container}>
-      <FlatList
-        data={posts}
-        renderItem={({ item }) => <FeedPost post={item} />}
-      />
+      <FeedScreen />
       <StatusBar style='auto' />
     </SafeAreaView>
   );
--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,7 +1,15 @@
 import { FeedPost } from '../components/FeedPost';
+import posts from '../../assets/data/posts.json';
+import { FlatList } from 'react-native';
 
 const FeedScreen = () => {
-  return <FeedPost post={post} />;
+  return (
+    <FlatList
+      showsVerticalScrollIndicator={false}
+      data={posts}
+      renderItem={({ item }) => <FeedPost post={item} />}
+    />
+  );
 };
 
 export default FeedScreen;
