a47e9c9893d1b8541496d743824536ff28e2a7cd
Anıl <anil@Anl-MacBook-Air.local>
2024-02-23 12:59:28 +0300

    navigator

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { View, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import Navigator from './navigation';
+import Navigator from './src/navigation';
 
 export default function App() {
   return (
     <View styles={styles.container}>
       <Navigator />
-      <StatusBar style='auto' />
     </View>
   );
 }
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,14 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
-import FeedScreen from '../src/screens/FeedScreen';
+import FeedScreen from '../screens/FeedScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import CreatePostScreen from '../src/screens/CreatePostScreen';
+import CreatePostScreen from '../screens/CreatePostScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Feed'>
         <Stack.Screen name='Feed' component={FeedScreen} />
         <Stack.Screen name='Create Post' component={CreatePostScreen} />
       </Stack.Navigator>
